26866caacf75321eb55c964adad227b3fbf9871b
kinnikudaiki <gbhiroki1121@gmail.com>
2020-10-10 16:56:25 +0900

    create addEventListener

--- button/JsvaScriptの導入/main.js
+++ button/JsvaScriptの導入/main.js
@@ -1,1 +1,17 @@
-alert('hello')
+const btn = document.querySelector('#btn');
+
+const hello = function() {
+  // alert("hello");
+  btn.style.color = "red"
+}
+const hello2 = function() {
+  alert("hello");
+}
+
+btn.addEventListener('click', hello)
+btn.addEventListener('mouseenter', hello)
+
+btn.removeEventListener("mouseenter", hello)
+
+btn.addEventListener("click", hello2);
+
