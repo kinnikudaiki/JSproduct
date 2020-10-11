321bd9683193a2a2e14c50c62f4935aa70198784
kinnikudaiki <gbhiroki1121@gmail.com>
2020-10-11 19:56:31 +0900

    study method & propaty

--- button/JsvaScriptの導入/main.js
+++ button/JsvaScriptの導入/main.js
@@ -1,17 +1,20 @@
 const btn = document.querySelector('#btn');
 
-const hello = function() {
-  // alert("hello");
-  btn.style.color = "red"
-}
-const hello2 = function() {
-  alert("hello");
-}
+// const hello = function() {
+//   // alert("hello");
+//   btn.style.color = "red"
+// }
+// const hello2 = function() {
+//   alert("hello");
+// }
 
-btn.addEventListener('click', hello)
-btn.addEventListener('mouseenter', hello)
+// btn.addEventListener('click', hello)
+// btn.addEventListener('mouseenter', hello)
 
-btn.removeEventListener("mouseenter", hello)
+// btn.removeEventListener("mouseenter", hello)
 
-btn.addEventListener("click", hello2);
+// btn.addEventListener("click", hello2);
 
+let hello = "hello world";
+
+console.log(hello.split(""));
