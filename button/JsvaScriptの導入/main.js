978d268f2505915b09bc7a92d3c59c4c8396a5ef
kinnikudaiki <gbhiroki1121@gmail.com>
2020-10-11 21:39:26 +0900

    object study

--- button/JsvaScriptの導入/main.js
+++ button/JsvaScriptの導入/main.js
@@ -25,12 +25,61 @@ const btn = document.querySelector('#btn');
 // console.log(hello.length);
 
 // const arry = new Array(1,2,3,4,5,6);
-const arry = [1,2,3,4,5,6];
-// arry[2] = "go"
-// arry.push("new item");
-arry.unshift("new item");
-const val = arry.pop();
-const val2 = arry.shift();
-console.log(arry);
-console.log(val)
-console.log(val2)
+// const arry = [1,2,3,4,5,6];
+// // arry[2] = "go"
+// // arry.push("new item");
+// arry.unshift("new item");
+// const val = arry.pop();
+// const val2 = arry.shift();
+// console.log(arry);
+// console.log(val)
+// console.log(val2)
+
+// const person = {
+//   name: ["Code", "mafia"],
+//   age: 32,
+//   gender: "male",
+//   interests: {
+//     sports: "soccer",
+//     music: "piano"
+//   }
+// };
+
+// person.age = 12;
+// console.log(person.interests);
+
+// const arry = [1,2,3,4,5];
+
+// for(let i = 0; i < arry.length; i += 1){
+//   console.log(arry[i]);
+// }
+
+// for(let i in arry){
+//   console.log(1, arry[i]);
+// }
+
+// for(let v of arry){
+//   console.log(v);
+// }
+
+const todos = [
+  {
+    id:1,
+    title: "Go to school",
+    completed: true
+  },
+  {
+    id:2,
+    title: "Go to museum",
+    completed: true
+  },
+  {
+    id:3,
+    title: "Go shopping",
+    completed: false
+  }
+]
+
+for(let i = 0; i < todos.length; i++){
+  console.log(todos[i].title);
+}
